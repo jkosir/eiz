b91544c5c97100b1ec4c488c7f9fc47b651b7ab0
Jakob Kosir <kosir.jakob@gmail.com>
2017-12-01 17:45:52 +0100

    Piano.

--- src/components/NewCreativeDialog.vue
+++ src/components/NewCreativeDialog.vue
@@ -21,6 +21,16 @@
       height: 14px;
     }
   }
+
+  .piano-key {
+    transition: opacity 0.1s ease;
+  }
+  .piano-key:hover {
+    opacity: 0.8;
+  }
+  .piano-key:active {
+    opacity:0.5;
+  }
 </style>
 
 
@@ -42,22 +52,50 @@
           </div>
         </div>
         <div class="board-container">
-          <img class="platform" src="/assets/images/platform.gif" alt="" style="transform: translate(150px,191px)">
+          <img class="platform" src="/assets/images/platform.gif" alt="" style="transform: translate(150px,181px)">
           <img class="platform" src="/assets/images/platform.gif" alt="" style="transform: translate(250px,171px)">
           <img id="figure" src="/assets/images/mario.png" alt="" class="figure"
                :style="figureStyle">
           <img class="board" src="/assets/images/crtovje.PNG" alt="">
         </div>
+        <div class="piano">
+          <svg width="500px" viewBox="0 0 1120 400" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="piano-keyboard">
+              <g id="octave-1">
+                <rect id="octave-1-C-key" class="piano-key white-key" data-piano-key="C" stroke="#555555" fill="#FFFFF7" x="0" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-D-key" class="piano-key white-key" data-piano-key="D" stroke="#555555" fill="#FFFFF7" x="80" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-E-key" class="piano-key white-key" data-piano-key="E" stroke="#555555" fill="#FFFFF7" x="160" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-F-key" class="piano-key white-key" data-piano-key="F" stroke="#555555" fill="#FFFFF7" x="240" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-G-key" class="piano-key white-key" data-piano-key="G" stroke="#555555" fill="#FFFFF7" x="320" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-A-key" class="piano-key white-key" data-piano-key="A" stroke="#555555" fill="#FFFFF7" x="400" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-B-key" class="piano-key white-key" data-piano-key="B" stroke="#555555" fill="#FFFFF7" x="480" y="0" width="80" height="400"></rect>
+                <rect id="octave-1-C#-key" class="piano-key black-key" data-piano-key="C#" stroke="#979797" fill="#4B4B4B" x="60" y="0" width="40" height="280"></rect>
+                <rect id="octave-1-D#-key" class="piano-key black-key" data-piano-key="D#" stroke="#979797" fill="#4B4B4B" x="140" y="0" width="40" height="280"></rect>
+                <rect id="octave-1-F#-key" class="piano-key black-key" data-piano-key="F#" stroke="#979797" fill="#4B4B4B" x="300" y="0" width="40" height="280"></rect>
+                <rect id="octave-1-G#-key" class="piano-key black-key" data-piano-key="G#" stroke="#979797" fill="#4B4B4B" x="380" y="0" width="40" height="280"></rect>
+                <rect id="octave-1-A#-key" class="piano-key black-key" data-piano-key="A#" stroke="#979797" fill="#4B4B4B" x="460" y="0" width="40" height="280"></rect>
+              </g>
+              <g id="octave-2" transform="translate(560.000000, 0.000000)">
+                <rect id="octave-2-C-key" class="piano-key white-key" data-piano-key="C" stroke="#555555" fill="#FFFFF7" x="0" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-D-key" class="piano-key white-key" data-piano-key="D" stroke="#555555" fill="#FFFFF7" x="80" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-E-key" class="piano-key white-key" data-piano-key="E" stroke="#555555" fill="#FFFFF7" x="160" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-F-key" class="piano-key white-key" data-piano-key="F" stroke="#555555" fill="#FFFFF7" x="240" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-G-key" class="piano-key white-key" data-piano-key="G" stroke="#555555" fill="#FFFFF7" x="320" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-A-key" class="piano-key white-key" data-piano-key="A" stroke="#555555" fill="#FFFFF7" x="400" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-B-key" class="piano-key white-key" data-piano-key="B" stroke="#555555" fill="#FFFFF7" x="480" y="0" width="80" height="400"></rect>
+                <rect id="octave-2-C#-key" class="piano-key black-key" data-piano-key="C#" stroke="#979797" fill="#4B4B4B" x="60" y="0" width="40" height="280"></rect>
+                <rect id="octave-2-D#-key" class="piano-key black-key" data-piano-key="D#" stroke="#979797" fill="#4B4B4B" x="140" y="0" width="40" height="280"></rect>
+                <rect id="octave-2-F#-key" class="piano-key black-key" data-piano-key="F#" stroke="#979797" fill="#4B4B4B" x="300" y="0" width="40" height="280"></rect>
+                <rect id="octave-2-G#-key" class="piano-key black-key" data-piano-key="G#" stroke="#979797" fill="#4B4B4B" x="380" y="0" width="40" height="280"></rect>
+                <rect id="octave-2-A#-key" class="piano-key black-key" data-piano-key="A#" stroke="#979797" fill="#4B4B4B" x="460" y="0" width="40" height="280"></rect>
+              </g>
+            </g>
+          </svg>
+
+        </div>
         <div class="inner cover">
-          <h1 class="cover-heading">Vadnica glasbenih lestvic</h1>
-          <p class="lead">
-            Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-          <p class="lead">
-            <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-          </p>
           <button class="btn btn-default" @click="animate">Animate</button>
           <button class="btn btn-default" @click="animateFall">Animate fall</button>
-
         </div>
 
         <div class="mastfoot">
